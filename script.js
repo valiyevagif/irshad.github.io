2473e27e430d0ccf3f614a74b27f61bb5006745c
valiyevagif <130966252+valiyevagif@users.noreply.github.com>
2023-06-19 14:55:49 +0300

    script.js

--- script.js
+++ script.js
@@ -2,83 +2,86 @@ let quantity = document.querySelector('.quantity');
 let btnAdd = document.querySelectorAll('.btn-addtocart');
 let cartIcon = document.querySelector('.fa-cart-shopping');
 let cartSide = document.querySelector('.sidebar-cart');
-let buttonClose = document.querySelector('.btn-close');
-let cartUl = document.querySelector('.cart-items');
+let buttonClose = document.querySelector('.btn-close')
+let userName = document.querySelector('.username');
+let btnLogin = document.querySelector('.btnLogin');
 
-// Load cart quantity and items from localStorage
-let savedQuantity = localStorage.getItem('cartQuantity');
-let savedItems = localStorage.getItem('cartItems');
+btnLogin.addEventListener('click', () => {
+    let username = prompt("Please enter your username:");
+    let usernameElement = document.querySelector('h7');
+    usernameElement.innerHTML = `👋  <b>Xoş gəldin,</b> ${username}!`;
+    btnLogin.style.display = "none";
+})
 
-if (savedQuantity) {
-  quantity.textContent = savedQuantity;
-}
 
-if (savedItems) {
-  cartUl.innerHTML = savedItems;
-}
 
 btnAdd.forEach(button => {
-  button.addEventListener('click', () => {
-    let currentQuantity = Number(quantity.textContent);
-    currentQuantity++;
-    quantity.textContent = currentQuantity;
-  });
-});
+    button.addEventListener('click', () => {
+        let currentQuantity = Number(quantity.textContent);
+        currentQuantity++;
+        quantity.textContent = currentQuantity;
+    })
+
+})
 
 cartIcon.addEventListener('click', () => {
-  cartSide.classList.toggle('open');
-});
+    cartSide.classList.toggle('open');
 
-buttonClose.addEventListener('click', () => {
-  cartSide.classList.remove('open');
-});
+    buttonClose.addEventListener('click', () => {
+        cartSide.classList.remove('open');
+    })
+})
+
 
 btnAdd.forEach(button => {
-  button.addEventListener('click', () => {
-    let productContainer = button.closest('.product-container');
-    let productImg = productContainer.querySelector('img').src;
-    let productName = productContainer.querySelector('h4').textContent;
-    let productPrice = productContainer.querySelector('p').textContent;
+    button.addEventListener('click', () => {
+        let productContainer = button.closest('.product-container');
+        let productImg = productContainer.querySelector('img').src;
+        let productName = productContainer.querySelector('h4').textContent;
+        let productPrice = productContainer.querySelector('p').textContent;
+        let cartUl = document.querySelector('.cart-items');
 
-    let cartItem = document.createElement('li');
-    cartItem.innerHTML = `
-      <div class="cart-item-container">
+        let cartItem = document.createElement('li');
+
+        cartItem.innerHTML = `
+        
+        <div class="cart-item-container">
         <div class="left-side">
-          <div class="image-container">
-            <img src="${productImg}" alt="">
-          </div>
-          <div class="product-info">
-            <h4>${productName}</h4>
-            <p>${productPrice}</p>
-          </div>
+        <div class="image-container">
+        <img src="${productImg}" alt="">
+        </div>
+        <div class="product-info">
+        <h4>${productName}</h4>
+        <p>${productPrice}</p>
+        </div>
         </div>
         <div class="right-side">
-          <button class="btn-cartDelete">Səbətdən sil</button>
+        <button class="btn-cartDelete">Səbətdən sil</button>
         </div>
-      </div>
-    `;
-    cartUl.appendChild(cartItem);
-
-    // Save cart items to localStorage
-    localStorage.setItem('cartItems', cartUl.innerHTML);
-  });
-});
+        </div>
+        
+        `;
+        cartUl.appendChild(cartItem);
+    })
+})
 
 document.addEventListener('click', (e) => {
-  if (e.target.classList.contains('btn-cartDelete')) {
-    let cartItem = e.target.closest('.cart-item-container');
-    cartItem.remove();
+    if (e.target.classList.contains('btn-cartDelete')) {
+        let cartItem = e.target.closest('.cart-item-container');
+        cartItem.remove();
 
-    let currentQuantity = Number(quantity.textContent);
-    currentQuantity--;
-    quantity.textContent = currentQuantity;
+        let currentQuantity = Number(quantity.textContent);
+        currentQuantity--;
+        quantity.textContent = currentQuantity;
+    }
 
-    // Save cart items to localStorage
-    localStorage.setItem('cartItems', cartUl.innerHTML);
-  }
 });
 
-window.addEventListener('beforeunload', () => {
-  // Save cart quantity to localStorage
-  localStorage.setItem('cartQuantity', quantity.textContent);
-});
+let btnHamburger = document.querySelector('.hamburger');
+let navMobile = document.querySelector('.nav-mobile-bar');
+
+btnHamburger.addEventListener('click', () => {
+    navMobile.classList.toggle('opened');
+})
+
+
